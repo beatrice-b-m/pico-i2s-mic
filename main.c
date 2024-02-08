65d35064c76d366b9aa5465dec9bfe123c1f5ea6
Beatrice Brown-Mulry <bbrownmulry@gmail.com>
2024-02-07 23:14:38 -0500

    initial adaptation/testing

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 #include "mic_i2s.h"
 
 // used pins:
-#define MIC_SCK_PIN   20
-#define MIC_WS_PIN    (MIC_SCK_PIN+1)
-#define MIC_DATA_PIN  22
-#define LED_PIN       25
+#define MIC_BCLK_PIN  10
+#define MIC_LRCL_PIN  11
+#define MIC_DOUT_PIN  12
+#define LED_PIN       13
 
 // recording config:
 #define MIC_PIO_NUM     0       // use pio0
@@ -17,7 +17,7 @@
 
 static void print_samples(uint32_t *samples, size_t num_samples)
 {
-  printf("=== LEFT CHANNEL SAMPLES =============\n");
+  // printf("=== LEFT CHANNEL SAMPLES =============\n");
   for (size_t i = 0; i < num_samples; i += 2) {
     printf("%d\n", (int16_t) ((samples[i] >> 15) & 0xffff));
   }
@@ -29,7 +29,7 @@ int main(void)
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
 
-  mic_i2s_init(MIC_PIO_NUM, MIC_DATA_PIN, MIC_SCK_PIN, MIC_SAMPLE_FREQ, MIC_NUM_SAMPLES);
+  mic_i2s_init(MIC_PIO_NUM, MIC_DOUT_PIN, MIC_BCLK_PIN, MIC_SAMPLE_FREQ, MIC_NUM_SAMPLES);
   mic_i2s_start();
   sleep_ms(3000);
 
@@ -42,13 +42,13 @@ int main(void)
     gpio_put(LED_PIN, 0);
     print_samples(samples, MIC_NUM_SAMPLES);
 
-    // blink LED a few times
-    for (int i = 0; i < 4; i++) {
-      gpio_put(LED_PIN, 1);
-      sleep_ms(250);
-      gpio_put(LED_PIN, 0);
-      sleep_ms(250);
-    }
-    sleep_ms(1000);
+    // // blink LED a few times
+    // for (int i = 0; i < 4; i++) {
+    //   gpio_put(LED_PIN, 1);
+    //   sleep_ms(250);
+    //   gpio_put(LED_PIN, 0);
+    //   sleep_ms(250);
+    // }
+    // sleep_ms(1000);
   }
 }
